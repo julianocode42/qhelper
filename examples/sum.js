e9fdaaf92f868f796fd3148f9198cb4ddfc3cbd9
Juliano Soares da Silva <juliano.code42@gmail.com>
2024-02-14 23:29:16 -0300

    exec script and connect to debugger

---
+++ examples/sum.js
@@ -0,0 +1,12 @@
+
+console.log("Stating...")
+
+let a = 1;
+let b = 1;
+
+debugger;
+
+console.log(`${a} + ${b} = ${a+b}`)
+
+console.log("Finished!")
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
 
-console.log("Hello TSWorld!")
+import Websocket from "ws";
+import axios from "axios";
+import { spawn } from "child_process";
+
+async function main() {
+  spawn("node",["--inspect-brk" ,"examples/sum.js"])
+
+  await new Promise((res, rej)=> {
+    setTimeout(res, 10000)
+  })
+  
+  let res = await axios.get("http://127.0.0.1:9229/json");
+
+  let endpoint = res.data[0].webSocketDebuggerUrl;
+  console.log(endpoint);
+
+  let ws = new Websocket(endpoint, { perMessageDeflate: false });
+  await new Promise((resolve) => ws.once("open", resolve));
+
+  console.log("connected!");
+
+  ws.on("message", (msg) => console.log(msg.toString()));
+
+  console.log("Sending Debugger.resume");
+  ws.send(JSON.stringify({
+    id: 1,
+    method: 'Debugger.enable'
+  }));
+}
+
+main();
